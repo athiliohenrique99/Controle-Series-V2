ea16a7a8243e5106dc49a9f038d10d2b7935a73a
Athilio Henrique <athiliohenrique98@gmail.com>
2022-10-23 23:48:37 -0300

    alterações

--- ControleSeries/app/Http/Controllers/SeriesController.php
+++ ControleSeries/app/Http/Controllers/SeriesController.php
@@ -3,15 +3,19 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\SeriesFormRequest;
-use App\Models\Episode;
-use App\Models\Season;
 use App\Models\Series;
+use App\Repositories\EloquentSeriesRepository;
 use Illuminate\Http\Request;
 // use illuminate\Http\
 use Illuminate\Support\Facades\DB;
+use App\Repositories\SeriesRepository;
 
 class SeriesController extends Controller
 {
+    public function __construct(SeriesRepository $repository)
+    {   
+    }
+
     public function index(Request $request)
     {
         $series = Series::all();
@@ -28,35 +32,35 @@ class SeriesController extends Controller
 
     public function store(SeriesFormRequest $request)
     {
-        $serie = DB::transaction(function () use ($request) {
+        // $serie = DB::transaction(function () use ($request) {
 
-            $serie = Series::create($request->all());
-            $seasons = [];
-            for ($i = 1; $i <= $request->seasonsQty; $i++) {
-                $seasons[] = [
-                    'series_id' => $serie->id,
-                    'number' => $i,
-                ];
-            }
-            Season::insert($seasons);
+        //     $serie = Series::create($request->all());
+        //     $seasons = [];
+        //     for ($i = 1; $i <= $request->seasonsQty; $i++) {
+        //         $seasons[] = [
+        //             'series_id' => $serie->id,
+        //             'number' => $i,
+        //         ];
+        //     }
+        //     Season::insert($seasons);
 
-            $episodes = [];
-            foreach ($serie->seasons as $season) {
-                for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
-                    $episodes[] = [
-                        'season_id' => $season->id,
-                        'number' => $j
-                    ];
-                }
-            }
-            Episode::insert($episodes);
+        //     $episodes = [];
+        //     foreach ($serie->seasons as $season) {
+        //         for ($j = 1; $j <= $request->episodesPerSeason; $j++) {
+        //             $episodes[] = [
+        //                 'season_id' => $season->id,
+        //                 'number' => $j
+        //             ];
+        //         }
+        //     }
+        //     Episode::insert($episodes);
 
-            return $serie;
-        });
+        //     return $serie;
+        // });
 
-
+        $serie = $this->repository->add($request);
         return redirect('/series')
-            ->with('mensagem.sucesso', "Série '{$serie->nome}' adicionada com sucesso");
+            ->with('mensagem.sucesso', "Série " . $serie->nome . " adicionada com sucesso");
     }
 
     public function destroy(Series $series)
---
+++ ControleSeries/app/Providers/SeriesRepositoriesProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Providers;
+
+use App\Models\Series;
+use App\Repositories\EloquentSeriesRepository;
+use Illuminate\Support\ServiceProvider;
+// use SeriesRepository;
+
+class SeriesRepositoriesProvider extends ServiceProvider
+{
+    public array $bindings = [
+        SeriesRepository::class => EloquentSeriesRepository::class
+    ];
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    // public function register()
+    // {
+    //     $this->app->bind(SeriesRepository::class, EloquentSeriesRepository::class);
+    // }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    // public function boot()
+    // {
+    //     //
+    // }
+}
